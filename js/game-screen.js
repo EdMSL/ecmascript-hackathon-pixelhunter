e5c18c619c4d48c1bc461b560b4022b0bc9c6400
EdMSL <eduard2011rus@gmail.com>
2019-02-27 10:53:41 +0700

    Changes on game-screen.js

--- js/game-screen.js
+++ js/game-screen.js
@@ -22,6 +22,14 @@ const getGameScreenElement = (question, state) => {
   const DOUBLE_GAME_SCREEN = 2;
   const TRIPLE_GAME_SCREEN = 3;
 
+  const SINGLE_SCREEN_TITLE = `<p class="game__task">Угадай, фото или рисунок?</p>`;
+  const DOUBLE_SCREEN_TITLE = `<p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>`;
+
+  const WIDE_WIDTH = 705;
+  const WIDE_HEIGHT = 455;
+  const STANDART_WIDTH = 468;
+  const STANDART_HEIGHT = 458;
+
   let gameScreenTemplate;
 
   const gameStatsTemplate = `
@@ -30,65 +38,40 @@ const getGameScreenElement = (question, state) => {
     </ul>
   `;
 
-  const oneImageGameScreenTemplate = `
-    <p class="game__task">Угадай, фото или рисунок?</p>
-    <form class="game__content  game__content--wide">
-      <div class="game__option">
-        <img src="${question[0].img}" alt="Option 1" width="705" height="455">
-        <label class="game__answer  game__answer--photo">
-          <input class="visually-hidden" name="question1" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer  game__answer--paint">
-          <input class="visually-hidden" name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
+  const getDefaultGameScreenTemplate = (title, width, height, isWide) => `
+    ${title}
+    <form class="game__content  game__content${(() => {
+    if (isWide) {
+      return `--wide`;
+    }
+    return ``;
+  }
+  )()}">
+      ${question.map((element, index) => `
+        <div class="game__option">
+          <img src="${element.img}" alt="Option ${index + 1}" width="${width}" height="${height}">
+          <label class="game__answer game__answer--photo">
+            <input class="visually-hidden" name="question${index + 1}" type="radio" value="photo">
+            <span>Фото</span>
+          </label>
+          <label class="game__answer game__answer--paint">
+            <input class="visually-hidden" name="question${index + 1}" type="radio" value="paint">
+            <span>Рисунок</span>
+          </label>
+        </div>
+      `).join(``)}
     </form>
     ${gameStatsTemplate}
   `;
 
-  const twoImagesGameScreenTemplate = `
-    <p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>
-    <form class="game__content">
-      <div class="game__option">
-        <img src="${question[0].img}" alt="Option 1" width="468" height="458">
-        <label class="game__answer game__answer--photo">
-          <input class="visually-hidden" name="question1" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer game__answer--paint">
-          <input class="visually-hidden" name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-      <div class="game__option">
-        <img src="${question[1].img}" alt="Option 2" width="468" height="458">
-        <label class="game__answer  game__answer--photo">
-          <input class="visually-hidden" name="question2" type="radio" value="photo">
-          <span>Фото</span>
-        </label>
-        <label class="game__answer  game__answer--paint">
-          <input class="visually-hidden" name="question2" type="radio" value="paint">
-          <span>Рисунок</span>
-        </label>
-      </div>
-    </form>
-    ${gameStatsTemplate}
-  `;
-
-  const threeImagesGameScreenTemplate = `
+  const getThreeImagesGameScreenTemplate = () => `
     <p class="game__task">Найдите рисунок среди изображений</p>
     <form class="game__content  game__content--triple">
-      <div class="game__option">
-        <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
-      </div>
-      <div class="game__option  game__option--selected">
-        <img src="http://placehold.it/304x455" alt="Option 2" width="304" height="455">
-      </div>
-      <div class="game__option">
-        <img src="http://placehold.it/304x455" alt="Option 3" width="304" height="455">
-      </div>
+      ${question.map((element, index) => `
+        <div class="game__option">
+          <img src="${element.img}" alt="Option ${index + 1}" width="304" height="455">
+        </div>
+      `).join(``)}
     </form>
     ${gameStatsTemplate}
   `;
@@ -96,19 +79,19 @@ const getGameScreenElement = (question, state) => {
   if (question.length === TRIPLE_GAME_SCREEN) {
     gameScreenTemplate = `
       <section class="game">
-        ${threeImagesGameScreenTemplate}
+        ${getThreeImagesGameScreenTemplate()}
       </section>
     `;
   } else if (question.length === DOUBLE_GAME_SCREEN) {
     gameScreenTemplate = `
       <section class="game">
-        ${twoImagesGameScreenTemplate}
+        ${getDefaultGameScreenTemplate(DOUBLE_SCREEN_TITLE, STANDART_WIDTH, STANDART_HEIGHT)}
       </section>
     `;
   } else if (question.length === SINGLE_GAME_SCREEN) {
     gameScreenTemplate = `
       <section class="game">
-        ${oneImageGameScreenTemplate}
+        ${getDefaultGameScreenTemplate(SINGLE_SCREEN_TITLE, WIDE_WIDTH, WIDE_HEIGHT, true)}
       </section>
     `;
   } else {
@@ -148,7 +131,7 @@ const getGameScreen = (question, state) => {
         });
         break;
     }
-    // gameState.level = gameState.level + 1;
+    gameState.level = gameState.level + 1;
   } else {
     gameContentForm.addEventListener(`change`, () => {
       renderScreen([getStatsScreen()]);
